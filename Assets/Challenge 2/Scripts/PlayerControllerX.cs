3f23361dcdfc684692302df212337899a8cb012f
billybep <billy.pesoth@gmail.com>
2022-11-09 13:03:08 +0800

    create spawn interval random, prevent player spam spawning dog

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,22 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    private float spawnDelay = 2.0f;
+    private float timer = 1.0f;
+
+
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            if (timer > spawnDelay)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                timer = 1.0f;
+            }
         }
+        timer += Time.deltaTime;
     }
 }
